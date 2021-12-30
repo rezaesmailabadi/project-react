6c4c6a55bc7d96c8434abdbfda1b6e54fc5c6e61
rezaesmailabadi <rezaesmailabadi@gmail.com>
2021-12-30 13:29:10 +0330

    tree-project

--- src/App.js
+++ src/App.js
@@ -5,15 +5,49 @@ import Main from "./Main";
 
 // قالب اصلی پروژه
 
+// ---------------------پروژه ی اول ----------------------
+// const App =()=>{
+//     return <div>
+//         <Header/>
+//         <Main/>
+//     </div>
+// };
 
-const App =()=>{
-    return <div>
-        <Header/>
-        <Main/>
-    </div>
-};
+// export default App;
+// ----------------------------------------------------
 
-export default App;
+
+
+// ---------------------------پروژه ی دوم ---------------------------
+class MyComponent extends React.Component{
+
+    state = {
+        conter : 35
+    }
+
+
+    incCounter =()=>{
+        console.log("counter clicked");
+        
+        this.setState({
+            counter : this.state.counter+1
+        });
+    }
+
+    render(){
+        return <div>
+            <p>{this.state.counter}</p>
+            <button onClick={this.incCounter}>شمارش</button>
+        </div>
+    }
+}
+
+export default MyComponent;
+
+
+
+
+
 
 
 
